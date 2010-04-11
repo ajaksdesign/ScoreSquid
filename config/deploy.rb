025d8a4433a36c90b262b285674089af187fd242
antony.koch@btinternet.com <antony.koch@btinternet.com>
2010-04-11 21:37:04 +0100

    adding capistrano stuff

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "ScoreSquid"
+set :repository,  "git@github.com:ajaksdesign/ScoreSquid.git"
+
+set :scm, "git"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user "ajaksdesign"
+set :scm_passphrase "0Requiem1"
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+set :branch "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
